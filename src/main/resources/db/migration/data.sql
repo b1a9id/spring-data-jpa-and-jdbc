54ab7ec018497c4c00a0005e39115be92004b4ad
RyosukueUchitate <ryosuke.uchitate@weblio.jp>
2024-11-30 14:32:27 +0900

    Add data.sql

---
+++ src/main/resources/db/migration/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO shop (name)
+VALUES ('shop-1'),
+       ('shop-2'),
+       ('shop-3'),
+       ('shop-4'),
+       ('shop-5');
+
+INSERT INTO user (name, age, shop_id)
+VALUES ('user-1', 10, 1),
+       ('user-2', 11, 2),
+       ('user-3', 12, 2),
+       ('user-4', 13, 3),
+       ('user-5', 14, 4),
+       ('user-6', 15, 4),
+       ('user-7', 16, 4),
+       ('user-8', 17, 5);
