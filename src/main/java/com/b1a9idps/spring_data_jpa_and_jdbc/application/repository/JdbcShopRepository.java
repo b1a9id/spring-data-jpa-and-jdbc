60c5f28f014b61e04002c4cbcb007c294c2603ed
RyosukueUchitate <ryosuke.uchitate@weblio.jp>
2024-12-01 13:35:34 +0900

    不要箇所削除

--- src/main/java/com/b1a9idps/spring_data_jpa_and_jdbc/application/repository/JdbcShopRepository.java
+++ src/main/java/com/b1a9idps/spring_data_jpa_and_jdbc/application/repository/JdbcShopRepository.java
@@ -1,9 +1,8 @@
 package com.b1a9idps.spring_data_jpa_and_jdbc.application.repository;
 
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.ListPagingAndSortingRepository;
 
 import com.b1a9idps.spring_data_jpa_and_jdbc.application.entity.JdbcShop;
 
-public interface JdbcShopRepository extends ListPagingAndSortingRepository<JdbcShop, Integer>, CrudRepository<JdbcShop, Integer> {
+public interface JdbcShopRepository extends ListPagingAndSortingRepository<JdbcShop, Integer> {
 }
--- src/main/java/com/b1a9idps/spring_data_jpa_and_jdbc/application/repository/JdbcUserRepository.java
+++ src/main/java/com/b1a9idps/spring_data_jpa_and_jdbc/application/repository/JdbcUserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.ListPagingAndSortingRepository;
 
 import com.b1a9idps.spring_data_jpa_and_jdbc.application.entity.JdbcUser;
 
-public interface JdbcUserRepository extends ListPagingAndSortingRepository<JdbcUser, Integer>, CrudRepository<JdbcUser, Integer> {
+public interface JdbcUserRepository extends ListPagingAndSortingRepository<JdbcUser, Integer> {
 }
