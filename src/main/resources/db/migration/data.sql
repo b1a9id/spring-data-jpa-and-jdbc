a50324b6d56d33ce76c5e88d1c7dbb598a3d3235
RyosukueUchitate <ryosuke.uchitate@weblio.jp>
2024-12-01 12:27:53 +0900

    Fix data.sql

--- src/main/resources/db/migration/data.sql
+++ src/main/resources/db/migration/data.sql
@@ -1,16 +1,16 @@
-INSERT INTO shop (name)
-VALUES ('shop-1'),
-       ('shop-2'),
-       ('shop-3'),
-       ('shop-4'),
-       ('shop-5');
+INSERT INTO shop (name, created_at, updated_at)
+VALUES ('shop-1', NOW(), NOW()),
+       ('shop-2', NOW(), NOW()),
+       ('shop-3', NOW(), NOW()),
+       ('shop-4', NOW(), NOW()),
+       ('shop-5', NOW(), NOW());
 
-INSERT INTO user (name, age, shop_id)
-VALUES ('user-1', 10, 1),
-       ('user-2', 11, 2),
-       ('user-3', 12, 2),
-       ('user-4', 13, 3),
-       ('user-5', 14, 4),
-       ('user-6', 15, 4),
-       ('user-7', 16, 4),
-       ('user-8', 17, 5);
+INSERT INTO user (name, age, shop_id, created_at, updated_at)
+VALUES ('user-1', 10, 1, NOW(), NOW()),
+       ('user-2', 11, 2, NOW(), NOW()),
+       ('user-3', 12, 2, NOW(), NOW()),
+       ('user-4', 13, 3, NOW(), NOW()),
+       ('user-5', 14, 4, NOW(), NOW()),
+       ('user-6', 15, 4, NOW(), NOW()),
+       ('user-7', 16, 4, NOW(), NOW()),
+       ('user-8', 17, 5, NOW(), NOW());
