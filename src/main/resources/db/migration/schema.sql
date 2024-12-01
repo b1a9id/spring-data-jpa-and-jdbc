4ae71271ae8ad9438196d14c803c3cd6b1baee37
RyosukueUchitate <ryosuke.uchitate@weblio.jp>
2024-12-01 12:16:34 +0900

    Fix schema.sql

--- src/main/resources/db/migration/schema.sql
+++ src/main/resources/db/migration/schema.sql
@@ -1,14 +1,20 @@
 CREATE TABLE IF NOT EXISTS shop
 (
-    id   INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
-);
+    id         INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name       VARCHAR(100) NOT NULL,
+    created_at DATETIME     NOT NULL,
+    updated_at DATETIME     NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
 CREATE TABLE IF NOT EXISTS user
 (
-    id      INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name    VARCHAR(100) NOT NULL,
-    age     INTEGER,
-    shop_id INTEGER,
-    FOREIGN KEY index_shop_id (shop_id) REFERENCES shop (id)
-);
+    id         INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name       VARCHAR(100) NOT NULL,
+    age        INTEGER,
+    shop_id    INTEGER,
+    created_at DATETIME     NOT NULL,
+    updated_at DATETIME     NOT NULL,
+    FOREIGN KEY index_shop_id (shop_id) REFERENCES shop (id) ON UPDATE RESTRICT ON DELETE RESTRICT
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
