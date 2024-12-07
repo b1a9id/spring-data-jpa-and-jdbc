0ff169d133e88487ca5bf7993358cb656e942d5a
RyosukueUchitate <ryosuke.uchitate@weblio.jp>
2024-12-07 17:26:39 +0900

    Fix

--- src/main/java/com/b1a9idps/spring_data_jpa_and_jdbc/application/repository/jdbc/package-info.java
+++ src/main/java/com/b1a9idps/spring_data_jpa_and_jdbc/application/repository/jdbc/package-info.java
@@ -1,5 +1,5 @@
 package com.b1a9idps.spring_data_jpa_and_jdbc.application.repository.jdbc;
 
 /**
- * Spring Data JPAを利用するレポジトリ
+ * Spring Data JDBCを利用するレポジトリ
  */
---
+++ src/main/java/com/b1a9idps/spring_data_jpa_and_jdbc/application/repository/jpa/package-info.java
@@ -0,0 +1,5 @@
+package com.b1a9idps.spring_data_jpa_and_jdbc.application.repository.jpa;
+
+/**
+ * Spring Data JPAを利用するレポジトリ
+ */
